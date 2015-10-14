27db3058c3accf5ec3a347cb2389cf38bf82ff8f
Kelsey McKenna <kelseyguitar55@googlemail.com>
2015-10-14 22:45:16 +0100

    python test files fixed

--- week-0/test_ex1.py
+++ week-0/test_ex1.py
@@ -2,15 +2,5 @@ import os, sys
 sys.path.insert(0, os.path.abspath('..'))
 import test, validator
 
-exercise_numbers = {
-	"exercise-1": test.one,
-	"exercise-2": test.two,
-	"problem": test.prob,
-}
-
 if __name__ == '__main__':
-
-	assert "exercise-1" in exercise_numbers, "Unknown exercise name."
-	testid = exercise_numbers["exercise-1"]
-
-	test.test('46042', '0', testid)
+	test.test('46042', '0', '1')
