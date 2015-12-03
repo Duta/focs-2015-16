cebcf20081ff9ed3451096cea6b7709978ab60b9
Kelsey McKenna <kelseyguitar55@googlemail.com>
2015-12-03 22:55:58 +0000

    wontfix: 10.2

--- week-10/test_ex2.py
+++ week-10/test_ex2.py
@@ -1,8 +1,1 @@
-import os, sys
-sys.path.insert(0, os.path.abspath('..'))
-import test, validator
-
-if __name__ == '__main__':
-  testIdTemp = 'exercise-2'
-  testId = testIdTemp[9:]
-  test.test('48364', '10', testId)
+print ("Please submit exercise 2 manually on Canvas.")
